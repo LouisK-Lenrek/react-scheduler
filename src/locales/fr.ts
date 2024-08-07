79e738ece317982ed95a244e1d1683497c7241f9
Louis-KRMK <louis.karamucki@lenrek.com>
2024-08-07 14:04:42 +0200

    Add FR-lang

---
+++ src/locales/fr.ts
@@ -0,0 +1,17 @@
+export const fr = {
+  feelingEmpty: "Le planning est vide.",
+  free: "Libre",
+  loadNext: "Suivant",
+  loadPrevious: "Previous",
+  over: "over",
+  taken: "Taken",
+  topbar: {
+    filters: "Filtrer",
+    next: "Suivant",
+    prev: "Précédent",
+    today: "Aujourd'hui",
+    view: "Zoom"
+  },
+  search: "Recherche",
+  week: "Semaine"
+};
